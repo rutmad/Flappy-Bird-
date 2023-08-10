cc436c3bea45c70562e25cd865fdcea87b1950fe
Yarin Eliyahu <yarinel2002@gmail.com>
2023-08-10 18:25:00 +0300

    update

--- team project/dist/server.js
+++ team project/dist/server.js
@@ -1,5 +1,9 @@
 var express = require('express');
 var app = express();
+app.use(express.static('public'));
+// app.get("/", (res:any, req:any) => {
+//   res.send()
+// });
 app.listen(3001, function () {
     console.log("running on port 3001");
 });
--- team project/server.ts
+++ team project/server.ts
@@ -2,7 +2,14 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.static('public'));
+
+// app.get("/", (res:any, req:any) => {
+//   res.send()
+// });
+
 app.listen(3001, () => {
   console.log("running on port 3001");
   
-})
+});
+
