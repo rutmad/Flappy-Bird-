3285e5c39c175a678ed5c38569d1a846aba6aa40
rutmad <122039144+rutmad@users.noreply.github.com>
2023-08-13 02:27:21 +0300

    RUT

--- team project/dist/server.js
+++ team project/dist/server.js
@@ -1,11 +1,25 @@
 "use strict";
 exports.__esModule = true;
 var express_1 = require("express");
+var mongoose_1 = require("mongoose");
+var dotenv = require("dotenv");
+var loginRoute_1 = require("./API/loginRoute");
+var cookie_parser_1 = require("cookie-parser");
+dotenv.config();
+var uri = process.env.MONGOOSE_URI + "FLAPPY-BIRD";
+if (uri) {
+    mongoose_1["default"]
+        .connect(uri)
+        .then(function () { return console.log("DB connected"); })["catch"](function (err) { return console.log("DB error :", err); });
+}
+else {
+    console.log("No URI");
+}
 var app = express_1["default"]();
-app.use(express_1["default"].static("public"));
-// app.get("/", (res:any, req:any) => {
-//   res.send()
-// });
-app.listen(3000, function () {
-    console.log("server listen on port 3000");
+app.use(express_1["default"].json());
+app.use(cookie_parser_1["default"]());
+app.use(express_1["default"].static("./client"));
+app.use("/api/", loginRoute_1["default"]);
+app.listen(3001, function () {
+    console.log("server listen on port 3001");
 });
--- team project/server.ts
+++ team project/server.ts
@@ -1,13 +1,30 @@
 import express from "express";
+import mongoose, { Schema } from "mongoose";
+import * as dotenv from "dotenv";
+import usersRoute from "./API/loginRoute";
+import cookieParser from "cookie-parser";
+
+dotenv.config();
+
+const uri: string | undefined = process.env.MONGOOSE_URI + "FLAPPY-BIRD";
+
+if (uri) {
+  mongoose
+    .connect(uri)
+    .then(() => console.log("DB connected"))
+    .catch((err) => console.log("DB error :", err));
+} else {
+  console.log("No URI");
+}
 
 const app = express();
+app.use(express.json());
+app.use(cookieParser());
 
-app.use(express.static("public"));
+app.use(express.static("./client"));
 
-// app.get("/", (res:any, req:any) => {
-//   res.send()
-// });
+app.use("/api/", usersRoute);
 
-app.listen(3000, () => {
-  console.log("server listen on port 3000");
+app.listen(3001, () => {
+  console.log("server listen on port 3001");
 });
