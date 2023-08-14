da3a2e935de8732a7b7ecbdce9c7efdf0ec7c027
orshkolnik <orshkolnik1@gmail.com>
2023-08-14 17:52:03 +0300

    or update

---
+++ team project/public/dist/obstacles.js
@@ -0,0 +1,25 @@
+var canvas = document.getElementById("gameCanvas");
+var ctx = canvas.getContext("2d");
+var PIPE_WIDTH = 50;
+var PIPE_SPACING = 150;
+var GRAVITY = 0.5;
+var Pipe = /** @class */ (function () {
+    function Pipe(x, y, height) {
+        this.x = x;
+        this.y = y;
+        this.height = height;
+    }
+    Pipe.prototype.draw = function () {
+        ctx.fillStyle = "green";
+        ctx.fillRect(this.x, this.y, PIPE_WIDTH, this.height);
+    };
+    return Pipe;
+}());
+var pipes = [];
+function createPipe() {
+    var minHeight = 50;
+    var maxHeight = canvas.height - minHeight - PIPE_SPACING;
+    var height = Math.random() * (maxHeight - minHeight) + minHeight;
+    pipes.push(new Pipe(canvas.width, 0, height));
+    pipes.push(new Pipe(canvas.width, height + PIPE_SPACING, canvas.height - (height + PIPE_SPACING)));
+}
---
+++ team project/public/obstacles.ts
@@ -0,0 +1,40 @@
+const canvas = document.getElementById("gameCanvas") as HTMLCanvasElement;
+const ctx = canvas.getContext("2d");
+
+const PIPE_WIDTH = 50;
+const PIPE_SPACING = 150;
+const GRAVITY = 0.5;
+
+class Pipe {
+  x: number;
+  y: number;
+  height: number;
+
+  constructor(x: number, y: number, height: number) {
+    this.x = x;
+    this.y = y;
+    this.height = height;
+  }
+
+  draw() {
+    ctx!.fillStyle = "green";
+    ctx!.fillRect(this.x, this.y, PIPE_WIDTH, this.height);
+  }
+}
+
+const pipes: Pipe[] = [];
+
+function createPipe() {
+  const minHeight = 50;
+  const maxHeight = canvas.height - minHeight - PIPE_SPACING;
+  const height = Math.random() * (maxHeight - minHeight) + minHeight;
+
+  pipes.push(new Pipe(canvas.width, 0, height));
+  pipes.push(
+    new Pipe(
+      canvas.width,
+      height + PIPE_SPACING,
+      canvas.height - (height + PIPE_SPACING)
+    )
+  );
+}
