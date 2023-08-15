57637596a0b410414ef4639b358e9ffcf1911868
rutmad <122039144+rutmad@users.noreply.github.com>
2023-08-15 14:12:31 +0300

    rut

--- team project/client/gameLevel.ts
+++ team project/client/gameLevel.ts
@@ -1,15 +1,15 @@
-async function handleGetUser() {
-  try {
-    const response = await fetch("/api/get-user");
-    const data = await response.json();
-    console.log("data", data);
-    const { user } = data;
-    const userHTML: HTMLDivElement | null = document.querySelector("#username");
+// async function handleGetUser() {
+//   try {
+//     const response = await fetch("/api/get-user");
+//     const data = await response.json();
+//     console.log("data", data);
+//     const { user } = data;
+//     const userHTML: HTMLDivElement | null = document.querySelector("#username");
 
-    if (!user) throw new Error("didn't get user from DB");
-    if (!userHTML) throw new Error("No user element on DOM");
-    userHTML.innerText = user.name;
-  } catch (error) {
-    console.error(error);
-  }
-}
+//     if (!user) throw new Error("didn't get user from DB");
+//     if (!userHTML) throw new Error("No user element on DOM");
+//     userHTML.innerText = user.name;
+//   } catch (error) {
+//     console.error(error);
+//   }
+// }
