081181d2f85ec1b74fb436c8352a7f6879abfa9f
orshkolnik <orshkolnik1@gmail.com>
2023-08-15 12:21:54 +0300

    or update

--- team project/public/dist/obstacles.js
+++ team project/public/dist/obstacles.js
@@ -1,7 +1,44 @@
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (_) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
 var canvas = document.getElementById("gameCanvas");
 var canvasContext = canvas.getContext("2d");
 var PIPE_WIDTH = 50;
-var PIPE_SPACING = 150;
+var PIPE_SPACING = 120;
+var PIPE_SPEED = 2;
 var Pipe = /** @class */ (function () {
     function Pipe(x, y, height) {
         this.x = x;
@@ -9,8 +46,19 @@ var Pipe = /** @class */ (function () {
         this.height = height;
     }
     Pipe.prototype.draw = function () {
-        canvasContext.fillStyle = "green";
-        canvasContext.fillRect(this.x, this.y, PIPE_WIDTH, this.height);
+        try {
+            if (!canvasContext) {
+                throw new Error("didn't found canvas");
+            }
+            canvasContext.fillStyle = "green";
+            canvasContext.fillRect(this.x, this.y, PIPE_WIDTH, this.height);
+        }
+        catch (error) {
+            console.log(error);
+        }
+    };
+    Pipe.prototype.move = function () {
+        this.x -= PIPE_SPEED;
     };
     return Pipe;
 }());
@@ -19,7 +67,59 @@ function createPipe() {
     var minHeight = 50;
     var maxHeight = canvas.height - minHeight - PIPE_SPACING;
     var height = Math.random() * (maxHeight - minHeight) + minHeight;
-    pipes.push(new Pipe(canvas.width, 0, height));
-    pipes.push(new Pipe(canvas.width, height + PIPE_SPACING, canvas.height - (height + PIPE_SPACING)));
+    var upperPipe = new Pipe(canvas.width, 0, height);
+    var lowerPipe = new Pipe(canvas.width, height + PIPE_SPACING, canvas.height - (height + PIPE_SPACING));
+    pipes.push(upperPipe, lowerPipe);
 }
-createPipe();
+function movePipe() {
+    for (var _i = 0, pipes_1 = pipes; _i < pipes_1.length; _i++) {
+        var pipe = pipes_1[_i];
+        pipe.move();
+    }
+}
+function drawPipe() {
+    for (var _i = 0, pipes_2 = pipes; _i < pipes_2.length; _i++) {
+        var pipe = pipes_2[_i];
+        pipe.draw();
+    }
+}
+function createPipeAndSetInterval() {
+    createPipe();
+    setInterval(createPipe, 2000);
+}
+createPipeAndSetInterval();
+function removePipes() {
+    for (var i = pipes.length - 1; i >= 0; i--) {
+        if (pipes[i].x + PIPE_WIDTH < 0) {
+            pipes.splice(i, 1);
+        }
+    }
+}
+function gameLoop() {
+    return __awaiter(this, void 0, void 0, function () {
+        var error_1;
+        return __generator(this, function (_a) {
+            switch (_a.label) {
+                case 0:
+                    _a.trys.push([0, 2, , 3]);
+                    if (!canvasContext) {
+                        throw new Error("there is no canvasContext");
+                    }
+                    canvasContext.clearRect(0, 0, canvas.width, canvas.height);
+                    return [4 /*yield*/, movePipe()];
+                case 1:
+                    _a.sent();
+                    drawPipe();
+                    removePipes();
+                    requestAnimationFrame(gameLoop);
+                    return [3 /*break*/, 3];
+                case 2:
+                    error_1 = _a.sent();
+                    console.log(error_1);
+                    return [3 /*break*/, 3];
+                case 3: return [2 /*return*/];
+            }
+        });
+    });
+}
+gameLoop();
--- team project/public/obstacles.ts
+++ team project/public/obstacles.ts
@@ -2,7 +2,8 @@ const canvas = document.getElementById("gameCanvas") as HTMLCanvasElement;
 const canvasContext = canvas.getContext("2d");
 
 const PIPE_WIDTH = 50;
-const PIPE_SPACING = 150;
+const PIPE_SPACING = 120;
+const PIPE_SPEED = 2;
 
 class Pipe {
   x: number;
@@ -16,8 +17,19 @@ class Pipe {
   }
 
   draw() {
-    canvasContext!.fillStyle = "green";
-    canvasContext!.fillRect(this.x, this.y, PIPE_WIDTH, this.height);
+    try {
+      if (!canvasContext) {
+        throw new Error("didn't found canvas");
+      }
+      canvasContext.fillStyle = "green";
+      canvasContext.fillRect(this.x, this.y, PIPE_WIDTH, this.height);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  move() {
+    this.x -= PIPE_SPEED;
   }
 }
 
@@ -28,14 +40,56 @@ function createPipe() {
   const maxHeight = canvas.height - minHeight - PIPE_SPACING;
   const height = Math.random() * (maxHeight - minHeight) + minHeight;
 
-  pipes.push(new Pipe(canvas.width, 0, height));
-  pipes.push(
-    new Pipe(
-      canvas.width,
-      height + PIPE_SPACING,
-      canvas.height - (height + PIPE_SPACING)
-    )
+  const upperPipe = new Pipe(canvas.width, 0, height);
+  const lowerPipe = new Pipe(
+    canvas.width,
+    height + PIPE_SPACING,
+    canvas.height - (height + PIPE_SPACING)
   );
+
+  pipes.push(upperPipe, lowerPipe);
 }
 
-createPipe();
+function movePipe() {
+  for (const pipe of pipes) {
+    pipe.move();
+  }
+}
+
+function drawPipe() {
+  for (const pipe of pipes) {
+    pipe.draw();
+  }
+}
+
+function createPipeAndSetInterval() {
+  createPipe();
+  setInterval(createPipe, 2000);
+}
+
+createPipeAndSetInterval();
+
+function removePipes() {
+  for (let i = pipes.length - 1; i >= 0; i--) {
+    if (pipes[i].x + PIPE_WIDTH < 0) {
+      pipes.splice(i, 1);
+    }
+  }
+}
+
+async function gameLoop() {
+  try {
+    if (!canvasContext) {
+      throw new Error("there is no canvasContext");
+    }
+    canvasContext.clearRect(0, 0, canvas.width, canvas.height);
+    await movePipe();
+    drawPipe();
+    removePipes();
+    requestAnimationFrame(gameLoop);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+gameLoop();
