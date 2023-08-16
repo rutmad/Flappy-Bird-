8474c559829aec29b09d378cb1b8ef876fbcf057
Yarin Eliyahu <yarinel2002@gmail.com>
2023-08-16 16:20:08 +0300

    Yarin-ScoreBoxUpdate

--- team project/public/box.ts
+++ team project/public/box.ts
@@ -1,11 +1,9 @@
-window.onload = function () {
-  const canvas = document.getElementById("gameCanvas") as HTMLCanvasElement;
-  const ctx = canvas.getContext("2d")!;
-  // Draw a black box
-  const boxWidth = 50;
-  const boxHeight = 50;
-  const boxX = 100; // X coordinate
-  const boxY = 100; // Y coordinate
-  ctx.fillStyle = "Black";
-  ctx.fillRect(boxX, boxY, boxWidth, boxHeight);
-};
+const box = document.querySelector(".black-box") as HTMLDivElement;
+
+if (box) {
+  box.innerHTML = `
+
+    0
+    
+    `;
+}
--- team project/public/dist/box.js
+++ team project/public/dist/box.js
@@ -1,11 +1,4 @@
-window.onload = function () {
-    var canvas = document.getElementById("gameCanvas");
-    var ctx = canvas.getContext("2d");
-    // Draw a black box
-    var boxWidth = 50;
-    var boxHeight = 50;
-    var boxX = 100; // X coordinate
-    var boxY = 100; // Y coordinate
-    ctx.fillStyle = "Black";
-    ctx.fillRect(boxX, boxY, boxWidth, boxHeight);
-};
+var box = document.querySelector(".black-box");
+if (box) {
+    box.innerHTML = "\n\n    0\n    \n    ";
+}
